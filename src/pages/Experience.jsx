2b47c61be17f06a3e0894e40e4f0ae3a4f1f64c2
Satria-WS <extencezone02@gmail.com>
2023-11-26 22:38:38 +0700

    fixing

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -13,7 +13,7 @@ const Experience = () => {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010-2014"
+          date="2006 - 2008"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
